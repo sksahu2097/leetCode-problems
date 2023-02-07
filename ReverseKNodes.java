8ed0948a41f0c3dd6fbd9b9a468354589ee15a13
sksahu2097 <sksahu2097@gmail.com>
2023-02-07 23:16:09 +0530

    K reverse linked list
    https://leetcode.com/problems/reverse-nodes-in-k-group/

---
+++ ReverseKNodes.java
@@ -0,0 +1,72 @@
+/**
+
+Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+
+k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+
+You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [2,1,4,3,5]
+Example 2:
+
+
+Input: head = [1,2,3,4,5], k = 3
+Output: [3,2,1,4,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 5000
+0 <= Node.val <= 1000
+
+https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev = dummy;
+        ListNode nex = dummy;
+
+        int count = 0;
+        while(curr != null) {
+            curr = curr.next;
+            count++;
+        }
+
+        while(count >= k) {
+            curr = prev.next; //first node of group
+            nex = curr.next; //second node of group
+            for(int i=1;i<k;i++) {
+                curr.next = nex.next;
+                nex.next = prev.next;
+                prev.next = nex;
+                nex = curr.next;
+            }
+            prev = curr;
+            count -= k;
+        }
+        
+
+        return dummy.next;
+        
+    }
+}

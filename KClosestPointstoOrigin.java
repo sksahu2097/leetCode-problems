c3926faf6c6f9f2f35244c752d9343fe1dbc7b66
Santosh Kumar Sahu <sksahu2097@gmail.com>
2023-03-06 20:57:52 +0530

    K Closest Points to Origin
    Calculate  the distance between two points on a plane is the Euclidean distance.
    
    https://leetcode.com/problems/k-closest-points-to-origin/description/

---
+++ KClosestPointstoOrigin.java
@@ -0,0 +1,60 @@
+/**
+
+973. K Closest Points to Origin
+
+Companies
+Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+ 
+
+Example 1:
+
+
+Input: points = [[1,3],[-2,2]], k = 1
+Output: [[-2,2]]
+Explanation:
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+Output: [[3,3],[-2,4]]
+Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+ 
+
+Constraints:
+
+1 <= k <= points.length <= 104
+-104 < xi, yi < 104
+
+https://leetcode.com/problems/k-closest-points-to-origin/description/
+
+*/
+
+class Solution {
+    public int[][] kClosest(int[][] A, int B) {
+        PriorityQueue<HashMap<String, Object>> queue = new PriorityQueue<>(
+				Comparator.comparingDouble(data -> (double)(data.get("dist"))));
+        int[][] answer = new int[B][A[0].length];
+        
+        for (int[] points : A) {
+            int x1 = points[0];
+            int y1 = points[1];
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("points", points);
+            map.put("dist", Math.sqrt(((x1-0)*(x1-0)) + ((y1-0)*(y1-0))));
+            queue.add(map);
+        }
+        for(int i=0;i<B;i++) {
+            answer[i] = (int[])queue.poll().get("points");
+        }
+        return answer;
+    }
+}
+
